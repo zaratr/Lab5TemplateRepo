75a67c9dc7b136dafdc35f8d8de859c171a741a6
zarat <zarate.rauljr@gmail.com>
2022-03-10 19:48:52 -0800

    uncommented the call on line 21

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ and the second element is a concatenated string that EXACTLY follows this exampl
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function sum(a = 4, b = 7) 
+function sum(a, b) 
 { //eslint-disable-line
     return
     [a + b,
@@ -18,7 +18,7 @@ function sum(a = 4, b = 7)
 }
 
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
