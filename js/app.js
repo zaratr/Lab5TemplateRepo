ee937b29c1d9f605bdd6a67d3d708636cc245435
raul <zarater.raul@gmail.com>
2022-03-10 20:29:47 -0800

    solved problem four

--- js/app.js
+++ js/app.js
@@ -76,9 +76,10 @@ function sumAndMultiply(a, b, c)
 
 /////////////////////////////////////
 /* Problem 4
-Write a function called sumArray() that takes in an array of numbers as its single argument and then returns an array where the first element is the sum of the numbers in the array, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
+Write a function called sumArray() that takes in an array of numbers as its single argument and then returns an array where the first element is 
+the sum of the numbers in the array, and the second element is a string that EXACTLY follows this example and uses the values that were input into 
+the function:
 
-"2,3,4 was passed in as an array of numbers, and 9 is their sum."
 
 IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To do addition, use your sum() function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
 
@@ -87,13 +88,18 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 let testArray = [2, 3, 4]; //eslint-disable-line
 
-function sumArray(sumArr) { //eslint-disable-line
-
+function sumArray(sumArr)
+{ //eslint-disable-line
+    return [
+        sumArr[0] + sumArr[1] + sumArr[2],
+        "2,3,4 was passed in as an array of numbers, and 9 is their sum."
+    ];
 }
+
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+ testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
