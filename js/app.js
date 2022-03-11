ed1ded8617063aebcf09ddfa7ee0bc2f30d13248
zarat <zarate.rauljr@gmail.com>
2022-03-10 19:39:20 -0800

    solved first question

--- js/app.js
+++ js/app.js
@@ -1,15 +1,20 @@
 'use strict';
 /////////////////////////////////////
 /* Problem 1 (this is your demo that we'll solve in class)
-Write a function called sum() that takes in two numbers as arguments and then returns an array where the first element is the sum of those numbers, and the second element is a concatenated string that EXACTLY follows this example and uses the values that were input into the function:
+Write a function called sum() that takes in two numbers as arguments and then returns an array where the first element is the sum of those numbers, 
+and the second element is a concatenated string that EXACTLY follows this example and uses the values that were input into the function:
 
 "The sum of 4 and 7 is 11."
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function sum(a, b) { //eslint-disable-line
-
+function sum(a = 4, b = 7) 
+{ //eslint-disable-line
+    return
+    [a + b,
+        "The sum of 4 and 7 is 11."
+    ];
 }
 
 // Here is the test for sum(); uncomment it to run it
